ac5da48e6cd101ad571b3cc7857746c899f30bb2
kawsarm104 <kawsarm104@gmail.com>
2021-10-21 15:15:11 +0600

    Dynamic id

--- beck/index.js
+++ beck/index.js
@@ -3,12 +3,43 @@ const cors = require("cors");
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const users = [
+  { id: 0, name: "Shabana", email: "Shabana@gmail.com", phone: "01788888888" },
+  {
+    id: 1,
+    name: "Shabnoor",
+    email: "Shabnoor@gmail.com",
+    phone: "01788888888",
+  },
+  {
+    id: 2,
+    name: "Shrabonti",
+    email: "Shrabonti@gmail.com",
+    phone: "01788888888",
+  },
+  {
+    id: 3,
+    name: "Suchorita",
+    email: "Suchorita@gmail.com",
+    phone: "01788888888",
+  },
+  { id: 4, name: "Soniya", email: "Soniya@gmail.com", phone: "01788888888" },
+  { id: 5, name: "Susmita", email: "Susmita@gmail.com", phone: "01788888888" },
+];
 
 app.get("/", (req, res) => {
   res.send("i am back with more knowledge, wow it's working ");
 });
-
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+app.get("/users/:id", (req, res) => { //Dynamic route
+    const searchId = req.params.id
+    const user = users[searchId]// array theke nitaci 1 ta user information re 
+    console.log(req.params.id);// ja patahaise tare khujtase 
+    res.send(user)
+});
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`server is runnig at port ${3001}`);
 });
